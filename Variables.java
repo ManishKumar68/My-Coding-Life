bedd81cf71370fd0aaf0149e32c7269cadd220ab
ManishKumar68 <manishkanojia79@gmail.com>
2025-12-21 22:12:01 +0530

    first class my java jur..

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+       System.out.println(a);
+       System.out.println(b);
+       String name = "Tony Stark";
+       System.out.println(name);
+       System.out.println("The sum is: " + (a + b));
+        
+
+        a = b;
+        System.out.println(a);
+     }
+}
