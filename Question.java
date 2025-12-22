6449ff5361225f7c807660d7369fbdcf186cf7bf
ManishKumar68 <manishkanojia79@gmail.com>
2025-12-22 15:12:27 +0530

    Question

---
+++ Question.java
@@ -0,0 +1,14 @@
+import java.util.*;
+// Question1:Inaprogram,input3numbers:A,BandC.Youhavetooutputtheaverageofthese 3 numbers.(Hint : Average of N numbers is sum of those numbers divided by N)
+
+
+public class Question {
+    public static void main(String[] args){
+
+    int A = 10;
+    int B = 20;
+    int C = 30;
+    int Average = (A + B + C) /3;
+    System.out.println("The average of 3 number is : " + Average);
+   }
+}
