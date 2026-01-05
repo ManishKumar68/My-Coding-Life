1d53efff85e62559daaa14746ff635497f7391bb
Manish Kumar <manishkanojia79@gmail.com>
2026-01-05 10:20:02 +0530

    Update Question.java

--- JavaLoops/Question.java
+++ JavaLoops/Question.java
@@ -223,7 +223,7 @@ public class Question {
 }*/
 
  
-// Question 12 : Write a program to find the factorialof any number entered by the user.(Hint:factorialofanumbern=n*(n-1)*(n-2)*(n-3)*......*1andex is ts for positive numbers only. We write factorial as n!So, factorial of 0! = 1, 1! = 1, 2! = 2, 3! = 6, 4! = 24 and so on.Note - Please do not confuse factorial with NOT EQUAL TO operator, they are not the same)
+// Question 12 : Write a program to find the factorialof any number entered by the user.(Hint:factorial of a number n=n*(n-1)*(n-2)*(n-3)*......*1andex is ts for positive numbers only. We write factorial as n!So, factorial of 0! = 1, 1! = 1, 2! = 2, 3! = 6, 4! = 24 and so on.Note - Please do not confuse factorial with NOT EQUAL TO operator, they are not the same)
 import java.util.Scanner;
 public class Question {
     public static void main(String[] args) {
@@ -238,3 +238,25 @@ public class Question {
     }
 }
 
+// Question 14 - Write a program to printthemultiplicationtable of a number N,entered by the user.
+import java.util.Scanner;
+ public class Question {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your naumber:");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++){
+            System.out.println(n + " * "  + i + " = " + n*i);
+        }
+    }
+}
+
+// Question 15 - What is wrong in the following program
+ public class Question {
+    public static void main(String [] args) {
+        for (int i = 0;
+         i <= 5; i++){
+            System.out.println("i = " + i) ;
+         }
+    }
+ }
