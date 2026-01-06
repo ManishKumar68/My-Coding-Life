f46b835a94a322cdb4bcd845978d0102f27dd74b
ManishKumar68 <manishkanojia79@gmail.com>
2026-01-06 09:39:02 +0530

    Partten

---
+++ Pattern/NestedLoops.java
@@ -0,0 +1,93 @@
+
+/*public class NestedLoops {
+    public static void main(String[] args) {
+        // outer loops
+        for (int Line = 1; Line <= 4; Line++) {
+            // inner Loops
+            for (int star = 1; star <= Line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/* output
+
+*
+**
+***
+****
+
+*/
+
+
+// inverted Star partten Print 
+/*public class NestedLoops {
+    public static void main(String[] args) {
+        int n = 4;
+        // outer loops
+        for (int Line = 1; Line <= 4; Line++) {
+            // inner Loops
+            for (int star = 1; star <= n-Line + 1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// NestedLoops output
+// ****
+// ***
+// **
+// *
+*/
+
+
+// Print Half-Pyramid pattern.
+/*public class NestedLoops {
+    public static void main(String[] args) {
+        
+        // outer loops
+        for (int Line = 1; Line <= 4; Line++) {
+            // inner Loops
+            for (int number = 1; number <= Line; number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+// Print Half-Pyramid Charecter Print...
+/*public class NestedLoops {
+    public static void main(String[] args) {
+        // outer loops
+        for (char i = 'A'; i <= 'J'; i++) {
+            // inner Loops
+            for (int Character = 'A'; Character <= i; Character++){
+                System.out.print((char)Character);
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+// Print Character Pattern..
+public class NestedLoops {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        // outer loops
+        for (int Line = 1 ; Line <= n; Line++) {
+            // inner Loops
+            for (int Character = 1; Character <= Line; Character++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ Pattern/Question.java
@@ -0,0 +1,265 @@
+// Question 1 - Hollow rectangle pattern
+/*public class Question{
+    public static void main(String args[]){
+        int Rows = 4;
+        int Cols = 5;
+        for (int i = 1; i <= Rows; i++){
+            for (int j = 1; j <= Cols; j++){
+
+                if (i == 1 || i == Rows || j == 1 || j == Cols){
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/* output
+
+    *  *  *  *  * 
+    *           * 
+    *           * 
+    *  *  *  *  * 
+    
+    */
+
+//  INVERTED & ROTATED HALF-PYRAMID pattern 
+ /*public class Question{
+    public static void main(String args[]){
+        
+        
+        for (int i = 1; i <=4 ; i++){
+                // inner Loops -  Print the Space.
+            for(int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+
+            // inner Loop -> Star Print 
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+// inverted Star partten Print 
+/*public class Question {
+    public static void main(String[] args) {
+        int n = 5;
+        int a = 1;
+        // outer loops
+        for (int Line = 1 ; Line <= n; Line++) {
+            // inner Loops
+            for (int Character = 1; Character <= Line; Character++){
+                System.out.print(a);
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}*/
+// Quesyion - 0-1 Triangle pattern
+ /*public class Question {
+    public static void main(String [] args){ 
+        int n = 5;
+        // Outter Lopp
+        for (int i = 1;  i <= n; i++){
+            // Ineer Loops
+            for (int j = 1; j <= i; j++){
+
+                if ((i + j) % 2 == 0){
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}*/
+
+// SOLID RHOMBUS pattern
+/*public class Question{
+    public static void main(String args[]){
+        int rows = 5;
+        
+        
+        for (int i = 1; i <= rows ; i++){
+                // inner Loops -  Print the Space.
+            for(int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // inner Loop -> Star Print 
+            for (int j = 1; j <= rows; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+// HOLLOW RHOMBUS Pattern
+/*public class Question {
+    public static void main(String [] args){
+        int n = 5;
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++){                
+                if (i == 1 ||i == n || j == 1|| j == n) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/* output
+
+    *****
+   *   *
+  *   *
+ *   *
+*****
+
+*/
+
+// DIAMOND Pattern
+/*public class Question {
+    public static void main (String[] args){
+        int row = 5;
+        int mid = row/2;
+
+        // Upper half
+        for (int i = 0; i <= mid; i++) {
+
+            for (int j = 1; j <= mid - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+      // Lower half
+        for (int i = mid - 1; i >= 0; i--) {
+
+            for (int j = 1; j <= mid - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+// NUMBER PYRAMID pattern
+/*public class Question {
+    public static void main (String[] args){
+        
+        int n = 5;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // assending number
+            for( int j = 1; j <= i ; j++){
+                System.out.print(j);
+            }
+
+            // decending Number
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+// NUMBER PYRAMID pattern-
+/*public class Question {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}*/
+
+public class Question {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            // left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n; i >= 1; i--) {
+
+            // left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
